a8c719ea085304fcc5ab565ad3da2afd6043c08b
Abhay Mishra <mishraabhay9179913420@gmail.com>
2021-04-08 00:58:48 +0530

    New system

--- welcome.sh
+++ welcome.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 greeting="Welcome"
-user="Abhay Mishra"
 day=$(date +%A )
 year=$(date +%Y)
 month=$(date +%B)
 digit=$(date +%d)
 
-echo "$greeting back $user!. The time is $(date +%r) and the date is $month $digit $year.
+echo "$greeting back ${whoami}!. The time is $(date +%r) and the date is $month $digit $year.
 Today is $day which is your favourite day.
-Wait Does that mean every day is your favourte day? "
+Wait Does that mean every day is your favourite day? "
